316bd2efc8a6e97d472490f814215617595f7b3c
Majid Ali <majid@teresol.com>
2024-09-24 00:49:52 +0500

    docs updates

--- src/generic-components/Helemt.jsx
+++ src/generic-components/Helemt.jsx
@@ -1,64 +1,36 @@
-import { Helmet } from "react-helmet";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 function Helemt_SEO() {
 	const devName = import.meta.env.VITE_APP_DEV_NAME || "Majid Ali";
 
 	return (
-		<Helmet>
-			<title>{`${devName} - Portfolio`}</title>
-			<meta
-				name="description"
-				content={`Welcome to ${devName}'s portfolio. Explore projects, services, and more.`}
-			/>
-			<meta
-				name="keywords"
-				content={`${devName}'s portfolio, ${devName}, Vue JS Developer, React JS Developer, Front End Developer, Portfolio, Web Developer, Software Engineer, Projects, Services`}
-			/>
-			<meta
-				name="author"
-				content={devName}
-			/>
+		<HelmetProvider>
+			<Helmet>
+				<title>{`${devName} - Portfolio`}</title>
+				<meta name="description" content={`Welcome to ${devName}'s portfolio. Explore projects, services, and more.`} />
+				<meta name="keywords" content={`${devName}'s portfolio, ${devName}, Vue JS Developer, React JS Developer, Front End Developer, Portfolio, Web Developer, Software Engineer, Projects, Services`} />
+				<meta name="author" content={devName} />
 
-			{/* Open Graph Meta Tags */}
-			<meta
-				property="og:title"
-				content={`${devName} - Portfolio`}
-			/>
-			<meta
-				property="og:description"
-				content={`Welcome to ${devName}'s portfolio. Explore projects, services, and more.`}
-			/>
-			<meta
-				property="og:image"
-				content="/soft-dev.png" // Ensure the image is located in the public directory
-			/>
-			<meta
-				property="og:url"
-				content={window.location.origin}
-			/>
-			<meta
-				property="og:type"
-				content="website"
-			/>
+				{/* Open Graph Meta Tags */}
+				<meta property="og:title" content={`${devName} - Portfolio`} />
+				<meta property="og:description" content={`Welcome to ${devName}'s portfolio. Explore projects, services, and more.`} />
+				<meta
+					property="og:image"
+					content="/soft-dev.png" // Ensure the image is located in the public directory
+				/>
+				<meta property="og:url" content={window.location.origin} />
+				<meta property="og:type" content="website" />
 
-			{/* Twitter Card Meta Tags */}
-			<meta
-				name="twitter:card"
-				content="summary_large_image"
-			/>
-			<meta
-				name="twitter:title"
-				content={`${devName} - Portfolio`}
-			/>
-			<meta
-				name="twitter:description"
-				content={`Welcome to ${devName}'s portfolio. Explore projects, services, and more.`}
-			/>
-			<meta
-				name="twitter:image"
-				content="/soft-dev.png" // Ensure the image is located in the public directory
-			/>
-		</Helmet>
+				{/* Twitter Card Meta Tags */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content={`${devName} - Portfolio`} />
+				<meta name="twitter:description" content={`Welcome to ${devName}'s portfolio. Explore projects, services, and more.`} />
+				<meta
+					name="twitter:image"
+					content="/soft-dev.png" // Ensure the image is located in the public directory
+				/>
+			</Helmet>
+		</HelmetProvider>
 	);
 }
 
