b85501c78876ff24ba2ddd8bd5d057db50e17513
Majid Ali <majid@teresol.com>
2024-10-07 00:55:28 +0500

    some props validations added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,22 +5,20 @@ import { Element } from "react-scroll";
 import { experienceData } from "../assets/experienceData";
 import SectionHeader from "../generic-components/Section-Header";
 import { getExperienceYear } from "../utils/getExpYears";
-
+import PropTypes from "prop-types";
 // Subcomponent to display each experience item
 const ExperienceItem = ({ description, highlightQuery }) => (
 	<ListItem>
-		<ListIcon
-			as={CheckCircleIcon}
-			color="green.500"
-		/>
-		<Highlight
-			query={highlightQuery}
-			styles={{ px: "2", color: "blue.500" }}>
+		<ListIcon as={CheckCircleIcon} color="green.500" />
+		<Highlight query={highlightQuery} styles={{ px: "2", color: "blue.500" }}>
 			{description}
 		</Highlight>
 	</ListItem>
 );
-
+ExperienceItem.propTypes = {
+	description: PropTypes.string.isRequired,
+	highlightQuery: PropTypes.string
+};
 export default function Experience() {
 	const fadeInUp = {
 		hidden: { opacity: 0, y: 20 },
@@ -29,41 +27,20 @@ export default function Experience() {
 
 	return (
 		<Element id="experience">
-			<Box
-				textAlign="left"
-				mb="8">
-				<SectionHeader
-					heading="Experience"
-					textAlign="left"
-				/>
+			<Box textAlign="left" mb="8">
+				<SectionHeader heading="Experience" textAlign="left" />
 			</Box>
-			<motion.div
-				initial="hidden"
-				whileInView="visible"
-				variants={fadeInUp}
-				viewport={{ once: true }}>
-				<Stat
-					color="gray.700"
-					mb={6}>
+			<motion.div initial="hidden" whileInView="visible" variants={fadeInUp} viewport={{ once: true }}>
+				<Stat color="gray.700" mb={6}>
 					<StatNumber>Al Habib Banking System</StatNumber>
 					<StatLabel>Front End Developer at Teresol Pvt Ltd</StatLabel>
 					<StatHelpText>Oct 24, 2022 - Present ({getExperienceYear()})</StatHelpText>
 				</Stat>
 
-				<List
-					spacing={3}
-					color="gray.700">
+				<List spacing={3} color="gray.700">
 					{experienceData.map((item, index) => (
-						<motion.div
-							key={index}
-							initial="hidden"
-							whileInView="visible"
-							variants={fadeInUp}
-							viewport={{ once: true }}>
-							<ExperienceItem
-								description={item.description}
-								highlightQuery={item.highlightQuery}
-							/>
+						<motion.div key={index} initial="hidden" whileInView="visible" variants={fadeInUp} viewport={{ once: true }}>
+							<ExperienceItem description={item.description} highlightQuery={item.highlightQuery} />
 						</motion.div>
 					))}
 				</List>
